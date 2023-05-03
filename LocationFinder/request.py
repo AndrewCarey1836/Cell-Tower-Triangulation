66f271672bd06b02cae0a6078f4d1a91df42180b
Andrew Carey <andrewcarey@usf.edu>
2023-05-02 21:03:57 -0400

    changed timing advance. Removed some functions

--- LocationFinder/request.py
+++ LocationFinder/request.py
@@ -5,17 +5,20 @@ import datetime
 
 class requestLocationAPI:
     #make a request to unwired labs location api
+    def requestFromAPI(self, CID, MCC, MNC, LAC):
 
-    def requestFromAPI(self, CID, MCC, MNC, LAC, TYPE):
-        group = 0
-        # print("Current Cell: " + str(CID))
+        #print("Current Cell: " + str(CID))
+        
         url = "https://us1.unwiredlabs.com/v2/process"
 
-        # data to be searched
+        #data to be searched
+        key = "Your key here"
+        
+        
         payload = {
-            "token": "Your Key Here",
+        "token": key,
             "cells": [{
-                "radio": TYPE,
+                "radio": "lte",
                 "cid": CID,
                 "mcc": MCC,
                 "mnc": MNC,
@@ -23,42 +26,45 @@ class requestLocationAPI:
             }],
             "address": 1
         }
-        message = json.dumps(payload)
-        response = requests.request("POST", url, data=message)
-        # print(response.text)
-        responseList = response.text.split(",")
-        # print(responseList)
-        if responseList[0] == '{"status":"ok"':
-            # print("OK!")
 
-            # print the current amount of calls left to the api
+        message = json.dumps(payload)
+
+        response = requests.request("POST", url, data=message)
+
+        #print(response.text)
+
+        responseList = response.text.split(",")
+
+        #print(responseList)
+
+        if responseList[0] == '{"status":"ok"':
+            #print("OK!")
+
+            #print the current amount of calls left to the api
             balance = str(responseList[1])
             balance = balance[10:]
 
-            # print the lat
+            #print the lat
             lat = str(responseList[2])
             lat = lat[6:]
 
-            # print the long
+            #print the long
             long = str(responseList[3])
             long = long[6:]
 
-            # put the date added to database
+            #put the date added to database
             currentTime = datetime.datetime.now()
             time = str(currentTime)
 
-            # print to screen and file
+            #print to screen and file
             print("Requests Left: " + balance)
-            towerData = str(CID) + " " + str(MCC) + " " + str(MNC) + " " + str(
-                LAC) + " " + lat + " " + long + " " + time
-            group = group + 1
-            self.storeToFile("towerSearch", towerData, (group % 3))
-            if not lat or not long:
-                return None, None
-            else:
-                return lat, long
+            towerData = str(CID) + " " + str(MCC) + " " + str(MNC) + " " + str(LAC) + " " + lat + " " + long + " " + time
+            self.storeToFile("towerSearch", towerData)
+            return lat, long
 
-    def checkStored(self, fileName, CID, MCC, MNC):
+
+    def checkStored(self,fileName, CID, MCC, MNC):
+        
         #open the folder and file
         path = "./towerDB"
         basedir = os.path.dirname(path)
@@ -66,69 +72,66 @@ class requestLocationAPI:
         
         file = open(fName, "r")
 
+
         while True:
+
             #read line by line searching for towers
             next_line = file.readline()
+            
             if not next_line:
                 #add new tower
-                return False, -1, -1
+                return "false", -1, -1
+                
 
             towerData = next_line.split(" ")
-            print(towerData)
-            string1 = str(towerData[0])
-            if type(towerData[0]) == int:
-                # print(towerData[0])
-                # check if tower exists in tower database and ignore if it does
-                if str(CID) == str(towerData[0]) and str(MCC) == str(towerData[1]) and str(MNC) == str(towerData[2]):
-                    # add the new tower
-                    # print("Match Found!")
-                    return True, str(towerData[4]), str(towerData[5])
-            else:
-                towerData[:] = towerData[1:]
-
+            #print(towerData[0])
+            #check if tower exists in tower database and ignore if it does
+            if str(CID) == str(towerData[0]) and str(MCC) == str(towerData[1]) and str(MNC) == str(towerData[2]):
+                #add the new tower
+                #print("Match Found!")
+                return "true", str(towerData[4]), str(towerData[5])
+                
         return ""
                 
 
-    def storeToFile(self, fileName, data, group):
-        #set the name of the output file to history and the current time
-        #ensure output directory exists
-        path = "./towerDB"
-        basedir = os.path.dirname(path)
-        if not os.path.exists(basedir):
-            os.makedirs(basedir)
+    def storeToFile(self,fileName, data):
+            
+            #set the name of the output file to history and the current time
+            
+            #ensure output directory exists
+            path = "./towerDB"
+            basedir = os.path.dirname(path)
+            if not os.path.exists(basedir):
+                os.makedirs(basedir)
 
-        #create file
-        fName = path + "/" + fileName
-        open(fName, 'a').close()
+            #create file
+            fName = path + "/" + fileName
+            open(fName, 'a').close()
 
-        #read info from data file
-        # = open(fName, "w")
-        file = open(fName, "a+")
+            #read info from data file
+            #file = open(fName, "w")
+            file = open(fName, "a+")
 
-        #write to the file
-        file.write(data)
-        file.write("\n")
-        if group == 0:
-            file.write("*****************************************\n")
-        #file.write(out)
+            #write to the file
+            file.write(data)
+            file.write("\n")
+            #file.write(out)
 
-        print("Finished writing!")
+            print("Finished writing!")
 
-    def request(self, CID, MCC, MNC, LAC, TYPE):
+    def request(self, CID, MCC, MNC, LAC):
         
         if str(CID) != "empty" and str(CID) != "" and str(CID) != " ":
             #check database for tower
             inDatabase, lat, long = self.checkStored("towerSearch", CID, MCC, MNC)
-            if not inDatabase:
-                #make a request based on tower type
-                if TYPE == "lte" or TYPE == "nbiot":
-                    lat, long = self.requestFromAPI(CID, MCC, MNC, LAC, TYPE)
-                else:
-                    print("Tower Type Not Specified")
+            #print(CID)
+            if inDatabase == "false":
+                #make a request
+                lat, long = self.requestFromAPI(CID, MCC, MNC, LAC)
                 return lat, long
-            elif inDatabase:
+            elif inDatabase == "true":
                 #say in database
-                print("In database already!")
+                #print("In database already!")
                 return lat, long
             else:
                 print("Something went wrong while performing the API request!")
